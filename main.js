bf72a15b2d33b0402ff30dd36c81d87a2151ac01
powersleo <powersleo@gmail.com>
2021-11-15 23:05:08 -0800

    reorganized code

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+var ctx
+var game = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 960;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext("2d");
+    this.canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border:2px solid blue";
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    //this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function () {
+    clearInterval(this.interval);
+  },
+};
+function startGame() {
+  game.start();
+  ctx = game.context;
+  init(ctx);
+}
