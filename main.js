63611c4cfb52952d3cb8f82d1497ee082b840b28
powersleo <powersleo@gmail.com>
2021-11-17 00:39:57 -0800

    reformatted

--- main.js
+++ main.js
@@ -8,24 +8,11 @@ var game = {
     this.context = this.canvas.getContext("2d");
     this.canvas.style =
       "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto; border:2px solid blue";
-    var img = new Image();
-    var title = new Image();
-    title.src = "resources/Titlescreen.png";
-    img.src = "resources/download.jpg";
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    text = new TextMessage("test");
+    text.init(document.body);
     this.frameNo = 0;
-    img.addEventListener(
-      "load",
-      function () {
-        this.context.drawImage(img, 0, 0);
-      },
-      false
-    );
-    img.src = "resources/download.jpg";
-    this.context.drawImage(title, 0, 0);
-    this.context.drawImage(img, 0, 0);
-    this.context.drawImage(title, 0, 0, 0, 0, 0, 0, 0, 0);
-    //this.interval = setInterval(updateGambeArea, 20);
+  
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
